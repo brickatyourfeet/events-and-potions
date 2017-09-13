216c2ca0d47518f94fd4a2b5f345768282db4a8e
Wes Reid <bwreid@gmail.com>
2017-09-13 15:45:37 -0700

    Potential solution

--- scripts/main.js
+++ scripts/main.js
@@ -1,106 +1,53 @@
-// Tasks:
-// document.getElementById
-// Find just the title
-var title = document.getElementById('title')
-// what is happening
-var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
-var clickCounter = 0
-
-title.addEventListener('click', changeTitleColor)
-function changeTitleColor () {
-  console.log(colors[clickCounter], clickCounter)
-  title.style.color = colors[clickCounter]
-  if (colors[clickCounter] === colors[colors.length - 1]) {
-    clickCounter = 0
-  } else {
-    clickCounter++
-  }
-}
-
+var items = []
 var lis = document.querySelectorAll('li')
 for (var i = 0; i < lis.length; i++) {
   var li = lis[i]
   li.addEventListener('click', function (event) {
-    // Grab the thing we are clicking on that is attached to event (the target).
-    // It has an object of style and a property of color that we are manually setting
-    // to be the color blue. This ends up in the style property on the element itself.
-    event.target.style.color = 'blue'
-    event.target.className += ' selected'
+    var li = event.target
+    if (li.nodeName === 'I') li = li.parentElement
 
-    var selectedItems = document.querySelectorAll('.selected')
+    li.className = 'selected'
 
-    var myItemsAsAString = ''
+    var item = li.textContent.trim()
+    items.push(item)
 
-    for (var j = 0; j < selectedItems.length; j++) {
-      myItemsAsAString += selectedItems[j].textContent.trim() + ' '
-    }
-
-    document.querySelector('.alert.alert-info span').textContent = myItemsAsAString
+    var info = document.querySelector('.alert.alert-info span')
+    info.textContent = items.join(', ')
   })
 }
 
+var button = document.getElementById('mix')
+button.addEventListener('click', function () {
+  var selected = document.querySelectorAll('.selected')
+  for (var i = 0; i < selected.length; i++) {
+    selected[i].className = ''
+  }
 
+  var info = document.querySelector('.alert.alert-info span')
+  info.textContent = 'Nothing is selected yet.'
 
+  if (items.includes('Snakewood')) button.style.color = 'brown'
+  if (items.includes('Cypress')) button.style.color = 'white'
+  if (items.includes('Lavender')) button.style.color = 'indigo'
+  if (items.includes('Mandrake')) button.style.backgroundColor = 'beige'
+  if (items.includes('Datura')) button.style.backgroundColor = 'aqua'
+  if (items.includes('Yarrow Root')) button.style.backgroundColor = 'magenta'
 
+  var container = document.querySelector('.main')
+  if (items.includes('Twice Blessed Water')) container.style.borderStyle = 'dashed'
+  if (items.includes('Honeywater')) container.style.borderStyle = 'dotted'
+  if (items.includes('Leech Juice')) container.style.borderStyle = 'solid'
+  if (items.includes('Syrup of Arnica')) container.style.backgroundColor = 'beige'
+  if (items.includes('Morning Dew')) container.style.backgroundColor = 'aqua'
+  if (items.includes('Wormwood Essence')) container.style.backgroundColor = 'magenta'
 
+  var container = document.querySelector('body')
+  if (items.includes('Horned slug')) container.style.background = 'red'
+  if (items.includes('Eel Eye')) container.style.background = 'yellow'
+  if (items.includes('Cockroach')) container.style.background = 'green'
+  if (items.includes('Cat Hair')) container.style.background = 'blue'
+  if (items.includes('Crow Feathers')) container.style.background = 'maroon'
+  if (items.includes('Toe of Frog')) container.style.background = 'orange'
 
-
-
-
-
-
-
-
-
-// Change the text of the title
-// - Choose Your Magical Ingredients
-title.textContent = 'Choose Your Magical Ingredients'
-// title.innerHTML = 'Choose Your <b>Magical Ingredients</b>'
-// Change the style of the title
-// Overriding styles
-// title.style = 'background-color: purple;'
-// title.style = 'color: orange;'
-
-// Setting specific styles
-// title.style.backgroundColor = 'purple'
-// title.style.color = 'orange'
-// Add a class to the title
-// - Add a class of .princess-font to the title
-title.className = 'princess-font'
-// Add more classes with `+=` and a space
-// title.className += ' highlighted'
-
-// document.querySelectorAll
-// Find all the list items
-var allListItems = document.querySelectorAll('li')
-// Find all list items of a specific list
-// var plants = document.querySelector('.plants')
-// Add a class of .green to all .plants list items
-var plants = document.querySelectorAll('.plants li i')
-for (var i = 0; i < plants.length; i++) {
-  plants[i].className += ' green'
-}
-// Add a class of .blue to all .liquids list items
-var liquids = document.querySelectorAll('.liquids li i')
-for (var i = 0; i < liquids.length; i++) {
-  liquids[i].className += ' blue'
-}
-// Add a class of .red to all .animal-parts list items
-var animalParts = document.querySelectorAll('.animal-parts li i')
-for (var i = 0; i < animalParts.length; i++) {
-  animalParts[i].className += ' red'
-}
-
-// document.createElement
-// Create a new ingredient from scratch!
-// var li = document.createElement('li')
-// li.textContent = 'Eye of Newt'
-// Add it to one of the lists at the top
-// var ul = document.querySelector('ul')
-// ul.prepend(li)
-// Add it to one of the lists at the bottom
-// ul.append(li)
-// Add it to one of the lists in the middle
-// element.remove()
-// Find and remove specific items
-// document.querySelectorAll('li')[8].remove()
+  items.splice(0, items.length)
+})
